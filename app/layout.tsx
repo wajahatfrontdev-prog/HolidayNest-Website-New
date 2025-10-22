f535b5edf757bb73e5cf59a155b92362f7b9da04
wajahatfrontdev-prog <wajahatfrontdev@gmail.com>
2025-10-22 21:13:44 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Travel Nest',
+  description: 'Created By WajahatDev',
   generator: 'v0.app',
 }
 
